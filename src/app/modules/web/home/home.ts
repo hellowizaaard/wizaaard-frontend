c8d544e1abb0ede195e9e40561fab16106b3f052
arahman74 <arahman.hybritech65@gmail.com>
2026-01-01 22:46:09 +0600

    enhance design

--- src/app/modules/web/home/home.ts
+++ src/app/modules/web/home/home.ts
@@ -1,9 +1,10 @@
 import { Component, AfterViewInit } from '@angular/core';
-
+import { NgClass,CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
+  imports: [CommonModule],
   styleUrls: ['./home.css']
 })
 export class Home implements AfterViewInit {
@@ -21,5 +22,21 @@ export class Home implements AfterViewInit {
         item.classList.toggle('active');
       });
     });
+  }
+   menuItems = [
+    { label: 'Resume', image: '/img/resume.png' },
+    { label: 'Cards', image: '/img/cards.png' },
+    { label: 'Jobs', image: '/img/jobs.png' },
+    { label: 'Landingpage', image: '/img/landingpage.png' },
+    { label: 'Templates', image: '/img/templates.png' },
+    { label: 'Mentors', image: '/img/mentors.png' }
+  ];
+  
+  activeMenu = this.menuItems[0].label;
+  activeImage = this.menuItems[0].image;
+
+  selectMenu(item: any) {
+    this.activeMenu = item.label;
+    this.activeImage = item.image;
   }
 }
